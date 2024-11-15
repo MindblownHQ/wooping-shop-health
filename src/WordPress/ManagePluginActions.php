3f303cc55658cde16376f5759406e2a083139845
Luc Princen <lucprincen@users.noreply.github.com>
2024-11-15 12:35:10 +0100

    removes shop health from the network activate page

--- src/WordPress/ManagePluginActions.php
+++ src/WordPress/ManagePluginActions.php
@@ -17,7 +17,12 @@ class ManagePluginActions implements Hookable {
 	 * Registers the hooks for the menu items in the admin menu.
 	 */
 	public function register_hooks(): void {
+		
+		// Add settings link.
 		\add_action( 'plugin_action_links_' . \plugin_basename( \SHOP_HEALTH_FILE ), [ $this, 'add_settings_link' ], 100 );
+
+		// @temp Remove support for network activation 
+		\add_filter( 'all_plugins', [ $this, 'remove_plugin_in_network_context' ]);
 	}
 
 	/**
@@ -32,5 +37,22 @@ class ManagePluginActions implements Hookable {
 		\array_unshift( $links, $settings_link );
 
 		return $links;
+	}
+
+
+	/**
+	 * Remove Wooping Shop Health on the network plugins page
+	 * @temporary fix until there's full multisite support.
+	 * 
+	 */
+	public function remove_plugin_in_network_context( array $plugins ): array {
+		global $current_screen;
+
+		if( $current_screen->is_network ){
+			unset( $plugins[ 'wooping-shop-health/shop-health.php' ] );
+		}
+		
+		return $plugins;
+
 	}
 }
