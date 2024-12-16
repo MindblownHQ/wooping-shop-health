c42b7f2d5247b065fcc0c432c6caa7f4735a4727
Mark <mark@yourmark.nl>
2024-12-16 19:44:26 +0100

    Add span as exception node

--- assets/src/js/item-list.js
+++ assets/src/js/item-list.js
@@ -5,7 +5,8 @@ document.addEventListener( "DOMContentLoaded", () => {
 	const products = document.querySelectorAll( ".wsh-issues-table__product" );
 	[ ...products ].forEach( ( product ) => {
 		product.addEventListener( "click", ( e ) => {
-			if ( e.target.nodeName === "A" || e.target.nodeName === 'BUTTON' ) {
+			console.log( e );
+			if ( e.target.nodeName === "A" || e.target.nodeName === "BUTTON" || e.target.nodeName === "SPAN" ) {
 				return;
 			}
 			slideToggle( product.querySelector( ".wsh-issues-table__issues-container" ), 300, { display: "grid" } );
