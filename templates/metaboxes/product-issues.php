d63e286c718cbb8edfb1932de787fb4c5ea7d3f4
Luc Princen <lucprincen@users.noreply.github.com>
2024-12-18 11:20:01 +0100

    updated translation strings

--- templates/metaboxes/product-issues.php
+++ templates/metaboxes/product-issues.php
@@ -7,9 +7,9 @@ if ( $object->relevant_issues->isEmpty() ) {
 <div class="wsh-issues-table wsh-product-issues-table">
 <div class="wsh-issues-table__issues-container wsh-product-issues-table">
     <header>
-        <span class="wsh-issues__description"><?php echo esc_html__( 'Description', 'wooping-shop-health' );?></span>
-        <span class="wsh-issues__first-seen"><?php echo esc_html__( 'First seen', 'wooping-shop-health' );?></span>
-        <span class="wsh-issues__actions"><?php echo esc_html__( 'Actions', 'wooping-shop-health' );?></span>
+        <span class="wsh-issues__description"><?php esc_html_e( 'Description', 'wooping-shop-health' );?></span>
+        <span class="wsh-issues__first-seen"><?php esc_html_e( 'First seen', 'wooping-shop-health' );?></span>
+        <span class="wsh-issues__actions"><?php esc_html_e( 'Actions', 'wooping-shop-health' );?></span>
     </header>
     <div class="wsh-issues-table__issues">
 		<?php foreach ( $object->relevant_issues as $issue ) { ?>
@@ -19,7 +19,7 @@ if ( $object->relevant_issues->isEmpty() ) {
                     <a href="<?php echo \esc_attr( $issue->docs_link ); ?>" target="_blank"
                        class="<?php echo( ! is_null( $issue->docs_description ) ? 'docs_link has-tooltip' : 'docs_link' ); ?>">
                         <span class="dashicons dashicons-welcome-learn-more"></span>
-                        <span><?php echo __( 'Learn more', 'wooping-shop-health' ); ?></span>
+                        <span><?php esc_html_e( 'Learn more', 'wooping-shop-health' ); ?></span>
 						<?php if ( ! is_null( $issue->docs_description ) ): ?>
                             <span class="tooltip">
 								<?php echo $issue->docs_description; ?>
@@ -30,7 +30,7 @@ if ( $object->relevant_issues->isEmpty() ) {
 				<time datetime="<?php echo $issue->created_at->format( 'c' ); ?>" class="reported_on wsh-issues-table__cell"><?php echo $issue->created_at->format( 'd M Y' ); ?></time>
                 <button class="button wsh-button wsh-button--ghost ignore-issue-btn"
                         data-issue-id="<?php echo \absint( $issue->id ); ?>">
-                    <?php echo esc_html__( 'Ignore issue', 'wooping-shop-health' );?>
+                    <?php esc_html_e( 'Ignore issue', 'wooping-shop-health' );?>
                 </button>
             </div>
 		<?php } ?>
